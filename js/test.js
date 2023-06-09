42a7b233156e79b62b2d13c162ea2753c4bcaf09
Roman <zloyandmad@yandex.ru>
2023-06-09 20:50:17 +0200

    added functions

--- js/test.js
+++ js/test.js
@@ -1,13 +1,20 @@
-const length = 9;
-let result = '';
+// const length = 9;
+// let result = '';
 
-for (let i = 1; i < length; i++) {
+// for (let i = 1; i < length; i++) {
 
-	for (let j = 0; j < i; j++) {
-		result += '*';
-	}
+// 	for (let j = 0; j < i; j++) {
+// 		result += '*';
+// 	}
 
-	result += '\n';
-}
+// 	result += '\n';
+// }
 
-console.log(result);
+// console.log(result);
+
+
+const test = '12.4px';
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
